904ac2e56a44ac48b83c3203e0277fd46d956e0b
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2024-12-30 09:20:10 +0700

    feat: add delay 3s when redirect

--- src/scripts/redirect.js
+++ src/scripts/redirect.js
@@ -1,22 +1,26 @@
 const redirect = (key, url) => {
   const mainWeb = process.env.MAIN_WEB
-  switch (key) {
-    case "success":
-      location.href = url
-      break
-    case "home":
-      location.href = mainWeb
-      break
-    case "not-found":
-      location.href = `${mainWeb}/link-not-found`
-      break
-    case "expired":
-      location.href = `${mainWeb}/link-invalid`
-      break
-    default:
-      location.href = `${mainWeb}/link-server-error`
-      break
-  }
+  const delay = 3000 // 3 seconds
+
+  setTimeout(() => {
+    switch (key) {
+      case "success":
+        location.href = url
+        break
+      case "home":
+        // location.href = mainWeb
+        break
+      case "not-found":
+        location.href = `${mainWeb}/link-not-found`
+        break
+      case "expired":
+        location.href = `${mainWeb}/link-invalid`
+        break
+      default:
+        location.href = `${mainWeb}/link-server-error`
+        break
+    }
+  }, delay)
 }
 
 export default redirect
